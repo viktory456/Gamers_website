5d15e0885b51701dd7fc5ded7406cb712cf586fe
Viktoria <vikvons@gmail.com>
2021-09-02 16:21:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function() {
+    $('.redarrow').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    })
+})
+$(function() {
+    $('.redarrow2').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    })
+})
+$(function() {
+    $('.redarrow3').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    })
+})
